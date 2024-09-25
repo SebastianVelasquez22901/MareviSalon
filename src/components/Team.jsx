7528ceb90f3fc9226f937b8259acce40cfc49b18
SebastianVelasquez22901 <sbgamer43@gmail.com>
2024-09-24 19:11:24 -0600

    Actualizacion

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,7 +7,7 @@ export const Team = (props) => {
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>Nuestro equipo</h2>
           <p>
-          "Estilistas, Cosmesmetólogas y Lashistas profesionales" con experiencia de más de 8 años
+          "Estilistas, Cosmetólogas y Lashistas profesionales" con experiencia de más de 8 años
           </p>
         </div>
         <div id="row">
